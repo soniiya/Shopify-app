e0aedc841c0813db679e9fa62065e6fe93737623
Soniya Jaiswal <soniyajaiswal995@gmail.com>
2023-03-15 15:30:35 +0530

    first

---
+++ web/frontend/components/ProductsCard.jsx
@@ -0,0 +1,126 @@
+import { useState } from "react";
+import {
+  Card,
+  Heading,
+  TextContainer,
+  DisplayText,
+  TextStyle,
+} from "@shopify/polaris";
+import { Toast } from "@shopify/app-bridge-react";
+import { useAppQuery, useAuthenticatedFetch } from "../hooks";
+
+export function ProductsCard() {
+  const emptyToastProps = { content: null };
+  const [isLoading, setIsLoading] = useState(true);
+  const [toastProps, setToastProps] = useState(emptyToastProps);
+  const fetch = useAuthenticatedFetch();
+
+  const {
+    data,
+    refetch: refetchProductCount,
+    isLoading: isLoadingCount,
+    isRefetching: isRefetchingCount,
+  } = useAppQuery({
+    url: "/api/products/count",
+    reactQueryOptions: {
+      onSuccess: () => {
+        setIsLoading(false);
+      },
+    },
+  });
+
+  const toastMarkup = toastProps.content && !isRefetchingCount && (
+    <Toast {...toastProps} onDismiss={() => setToastProps(emptyToastProps)} />
+  );
+
+  const handlePopulate = async () => {
+    setIsLoading(true);
+    const response = await fetch("/api/products/create");
+
+    if (response.ok) {
+      await refetchProductCount();
+      setToastProps({ content: "5 products created!" });
+    } else {
+      setIsLoading(false);
+      setToastProps({
+        content: "There was an error creating products",
+        error: true,
+      });
+    }
+  };
+
+
+
+  console.log("hii");
+
+  const fetchCollections = async() =>{
+
+    try{
+      const response = await fetch("/api/collections/438657024279");
+      console.log(await response.json())
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
+  fetchCollections();
+
+
+  const fetchOrders = async()=>{
+    try{
+      const response = await fetch("/api/orders");
+      console.log(await response.json());
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
+  fetchOrders();
+
+
+  const fetchCustomers = async() =>{
+
+      const response = await fetch("/api/customers/6812305522967");
+      console.log(await response.json())
+    
+    // catch(error){
+    //   console.log(error);
+    // }
+  }
+
+  fetchCustomers();
+
+
+
+  return (
+    <>
+      {toastMarkup}
+      <Card
+        title="Product Counter"
+        sectioned
+        primaryFooterAction={{
+          content: "Populate 5 products",
+          onAction: handlePopulate,
+          loading: isLoading,
+        }}
+      >
+        <TextContainer spacing="loose">
+          <p>
+            Sample products are created with a default title and price. You can
+            remove them at any time.
+          </p>
+          <Heading element="h4">
+            TOTAL PRODUCTS
+            <DisplayText size="medium">
+              <TextStyle variation="strong">
+                {isLoadingCount ? "-" : data.count}
+              </TextStyle>
+            </DisplayText>
+          </Heading>
+        </TextContainer>
+      </Card>
+    </>
+  );
+}
